edd1131c6348f630df1f80928cb8c839d51829bc
Harrison Mitchell <harymitchell@gmail.com>
2015-11-04 22:48:54 -0500

    Added Google Plus Authentication and view for posting items.

---
+++ nodeDemoApp/public/javascripts/postItem.js
@@ -0,0 +1,61 @@
+// DOM Ready =============================================================
+$(document).ready(function() {
+
+    // Post item button click
+    $('#btnPostItem').on('click', postItem);
+
+});
+
+// Add User
+function postItem(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#postItem input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all item info into one object
+        var newItem = {
+            'name': $('#postItem fieldset input#inputItemName').val(),
+            'type': $('#postItem fieldset input#inputItemType').val(),
+            'location': $('#postItem fieldset input#inputItemLocation').val(),
+            'price': $('#postItem fieldset input#inputItemPrice').val()
+        }
+
+        // Use AJAX to post the object to our postItem service
+        $.ajax({
+            type: 'POST',
+            data: newItem,
+            url: '/postItem',
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            if (response.msg === '') {
+
+                // Clear the form inputs
+                $('#postItem fieldset input').val('');
+
+                // Update the table
+                populateTable();
+
+            }
+            else {
+
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
--- nodeDemoApp/routes/index.js
+++ nodeDemoApp/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport-google-oauth');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'E-Shoppe' });
+  res.render('index', { title: 'E-Shoppe Welcome', user: req.user });
 });
 
 /* GET login page. */
@@ -14,6 +15,16 @@ router.get('/login', function(req, res, next) {
 /* GET listings page. */
 router.get('/listings', function(req, res, next) {
   res.render('listings', { title: 'E-Shoppe Listings' });
+});
+
+/* GET postItem page. */
+router.get('/postItem', function(req, res, next) {
+  res.render('postItem', { title: 'E-Shoppe Add New Listing' });
+});
+
+/* GET admin page. */
+router.get('/admin', function(req, res, next) {
+  res.render('admin', { title: 'E-Shoppe Admin' });
 });
 
 /*
@@ -30,7 +41,23 @@ router.get('/listings/currentListings', function(req, res) {
 /*
  * POST to user cart.
  */
-router.post('/listings/addListingToCart/:id', function(req, res) {
+router.post('/listings/postItemToCart/:id', function(req, res) {
+  // TODO
+    var item = req.params.id;
+    console.log ("todo: add item to cart:"+item)
+    var db = req.db;
+    var collection = db.get('userlist');
+    //collection.insert(req.body, function(err, result){
+    //    res.send(
+    //        (err === null) ? { msg: '' } : { msg: err }
+    //    );
+    //});
+});
+
+/*
+ * POST to listings
+ */
+router.post('/postItem', function(req, res) {
   // TODO
     var item = req.params.id;
     console.log ("todo: add item to cart:"+item)
