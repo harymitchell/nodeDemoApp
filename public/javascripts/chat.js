ce5cefd379d1b1c80f49ffc2ecbb35350c8c6c4b
Harrison Mitchell <harymitchell@gmail.com>
2015-11-09 12:35:03 -0500

    Improved page html layout.
    Plugged in my code to a simplified bootstrap layout
    (https://github.com/twbs/bootstrap/tree/master/docs/examples/dashboard)

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,6 +3,7 @@ var socket = io();
 // DOM Ready =============================================================
 $(document).ready(function() {
     
+	$('#messages').scrollTop($('#messages')[0].scrollHeight);
 	$('form').submit(function(){
 		var message = $('#inputMessage').val()
 		console.log ("form submit: "+message)
@@ -12,6 +13,7 @@ $(document).ready(function() {
 	});
 	socket.on('chat message', function(msg){
 		$('#messages').append($('<li>').text(msg));
+		$('#messages').scrollTop($('#messages')[0].scrollHeight);
 	});
 });
 
