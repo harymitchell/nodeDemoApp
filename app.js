01426c9df60c8b10d92a2fbc4f3d93ebeb8bab73
Harrison Mitchell <harymitchell@gmail.com>
2015-11-20 16:53:10 -0500

    Added beginnings of Youtube auth strategy

--- app.js
+++ app.js
@@ -98,6 +98,18 @@ app.get('/auth/google',
     // function will not be called.
   });
 
+// GET /auth/youtube
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+app.get('/auth/youtube',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/youtube'] }),
+  function(req, res){
+    // The request will be redirected to Google for authentication, so this
+    // function will not be called.
+  });
+
 // GET /auth/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
